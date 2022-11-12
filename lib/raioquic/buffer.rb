2c7b623a2d2c2416ac6e0ddc6e5fe98d562820d2
Yusuke Nakamura <yusuke1994525@gmail.com>
2022-11-13 00:35:31 +0900

    Migrate aioquic/buffer

--- lib/raioquic/buffer.rb
+++ lib/raioquic/buffer.rb
@@ -1,79 +1,172 @@
 # frozen_string_literal: true
 
+require "stringio"
+require "forwardable"
+
 module Raioquic
   class Buffer
-    BufferReaderError = Class.new(StandardError)
+    extend Forwardable
 
-    def initialize(capacity: nil, data: nil)
+    BufferReadError = Class.new(StandardError)
+    BufferWriteError = Class.new(StandardError)
+    ValueError = Class.new(StandardError)
+
+    def_delegators :@buffer, :eof, :eof
+    def_delegators :@buffer, :tell, :tell
+
+    def initialize(capacity: nil, data: "")
       @position = 0 # bytes count
-      @buffer = IO::Buffer.for(data)
+      @buffer = StringIO.open(+data, "rb+:ASCII-8bit:ASCII-8BIT")
+      @capacity = capacity || @buffer.size
     end
 
     def dealloc
-      @buffer.free
+      # empty
+    end
+
+    def data
+      @buffer.string
+    end
+
+    def seek(offset)
+      check_read_bounds(offset)
+      @buffer.seek(offset)
+    rescue Errno::EINVAL
+      raise BufferReadError
     end
 
     # NOTE: "end" is reserved keyword in Ruby
     def data_slice(start:, ends:)
+      orig_str = @buffer.string
       if start < 0 || @buffer.size < start || ends < 0 || @buffer.size < ends || ends < start
-        raise BufferReaderError, "Read out of bounds"
+        raise BufferReadError, "Read out of bounds"
       end
 
-      @buffer.slice(start, ends - start).get_string
+      orig_str.byteslice(start, ends - start)
     end
 
-    def eof
-      @buffer.size == @position
+    # Pull bytes.
+    def pull_bytes(length)
+      raise BufferReadError if @buffer.size < 1
+      @buffer.read(length)
+    rescue EOFError, ArgumentError
+      raise BufferReadError
     end
 
-    def seek
-    end
-
-    def tell
-    end
-
-    def pull_bytes(length:)
-    end
-
+    # Pull an 8-bit unsigned integer.
     def pull_uint8
+      @buffer.readpartial(1).unpack1("C")
+    rescue EOFError
+      raise BufferReadError
     end
 
+    # Pull a 16-bit unsigned integer.
     def pull_uint16
+      @buffer.readpartial(2).unpack1("n")
+    rescue EOFError
+      raise BufferReadError
     end
 
+    # Pull a 32-bit unsigned integer.
     def pull_uint32
+      @buffer.readpartial(4).unpack1("N")
+    rescue EOFError
+      raise BufferReadError
     end
 
+    # Pull a 64-bit unsigned integer.
     def pull_uint64
+      @buffer.readpartial(8).unpack1("Q>")
+    rescue EOFError
+      raise BufferReadError
     end
 
-    def pull_unit_var
+    # Pull a QUIC variable-length unsigned integer.
+    def pull_uint_var
+      check_read_bounds(1)
+      first = pull_uint8
+      case first >> 6
+      when 0
+        first & 0x3f
+      when 1
+        check_read_bounds(1)
+        second = pull_uint8
+        ((first & 0x3f) << 8) | (second)
+      when 2
+        check_read_bounds(3)
+        second = pull_uint8
+        third = pull_uint8
+        forth = pull_uint8
+        ((first & 0x3f) << 24) | (second << 16) | (third << 8) | forth
+      else
+        check_read_bounds(7)
+        b2 = pull_uint8
+        b3 = pull_uint8
+        b4 = pull_uint8
+        b5 = pull_uint8
+        b6 = pull_uint8
+        b7 = pull_uint8
+        b8 = pull_uint8
+        ((first & 0x3f) << 56) | (b2 << 48) | (b3 << 40) | (b4 << 32) | (b5 << 24) | (b6 << 16) | (b7 << 8) | b8
+      end
     end
 
-    def push_bytes(value:)
+    def push_bytes(value)
+      raise BufferWriteError if value.bytesize > [@buffer.size, @capacity].max
+      @buffer << value
     end
 
-    def push_uint8(value:)
+    def push_uint8(value)
+      @buffer << [value].pack("C")
     end
 
-    def push_uint16(value:)
+    def push_uint16(value)
+      @buffer << [value].pack("n")
     end
 
-    def push_uint32(value:)
+    def push_uint32(value)
+      @buffer << [value].pack("N")
     end
 
-    def push_uint64(value:)
+    def push_uint64(value)
+      @buffer << [value].pack("Q>")
     end
 
-    def push_uint_var(value:)
+    def push_uint_var(value)
+      if value <= 0x3f
+        check_read_bounds(1)
+        push_uint8(value)
+      elsif value <= 0x3fff
+        check_read_bounds(2)
+        push_uint8((value >> 8) | 0x40)
+        push_uint8(value & 0xff)
+      elsif value <= 0x3fffffff
+        check_read_bounds(4)
+        push_uint8((value >> 24) | 0x80)
+        push_uint8((value >> 16) & 0xff)
+        push_uint8((value >> 8) & 0xff)
+        push_uint8(value)
+      elsif value <= 0x3fffffffffffffff
+        check_read_bounds(8)
+        push_uint8((value >> 56) | 0xc0)
+        push_uint8((value >> 48) & 0xff)
+        push_uint8((value >> 40) & 0xff)
+        push_uint8((value >> 32) & 0xff)
+        push_uint8((value >> 24) & 0xff)
+        push_uint8((value >> 16) & 0xff)
+        push_uint8((value >> 8) & 0xff)
+        push_uint8(value & 0xff)
+      else
+        raise ValueError, "Integer is too big for a variable-length integer"
+      end
     end
 
-    def encode_uint_var(value:)
+    def encode_uint_var(value)
     end
 
     # Return the number of bytes required to encode the given value
     # as a QUIC variable-length unsigned integer.
-    def size_uint_var(value:)
+    def size_uint_var(value)
       if value <= 0x3f
         return 1
       elsif value <= 0x3fff
@@ -83,8 +176,12 @@ module Raioquic
       elsif value <= 0x3fffffffffffffff
         return 8
       else
-        raise ArgumentError, "Integer is too big for a variable-length integer"
+        raise ValueError, "Integer is too big for a variable-length integer"
       end
+    end
+
+    private def check_read_bounds(length)
+      raise BufferReadError, "Read out of bounds" if [@buffer.size, @capacity].max < length
     end
   end
 end
--- test/raioquic/test_buffer.rb
+++ test/raioquic/test_buffer.rb
@@ -7,74 +7,230 @@ class TestRaioquicBuffer < Minitest::Test
     buf = Raioquic::Buffer.new(data: "\x08\x07\x06\x05\x04\x03\x02\x01")
     assert_equal "\x08\x07\x06\x05\x04\x03\x02\x01", buf.data_slice(start: 0, ends: 8)
     assert_equal "\x07\x06", buf.data_slice(start: 1, ends: 3)
-    assert_raises Raioquic::Buffer::BufferReaderError do
+    assert_raises Raioquic::Buffer::BufferReadError do
       buf.data_slice(start: -1, ends: 3)
     end
-    assert_raises Raioquic::Buffer::BufferReaderError do
+    assert_raises Raioquic::Buffer::BufferReadError do
       buf.data_slice(start: 0, ends: 9)
     end
-    assert_raises Raioquic::Buffer::BufferReaderError do
+    assert_raises Raioquic::Buffer::BufferReadError do
       buf.data_slice(start: 1, ends: 0)
     end
   end
 
   def test_pull_bytes
+    buf = Raioquic::Buffer.new(data: "\x08\x07\x06\x05\x04\x03\x02\x01")
+    assert_equal "\x08\x07\x06", buf.pull_bytes(3)
   end
 
   def test_pull_bytes_negative
+    buf = Raioquic::Buffer.new(data: "\x08\x07\x06\x05\x04\x03\x02\x01")
+    assert_raises Raioquic::Buffer::BufferReadError do
+      buf.pull_bytes(-1)
+    end
   end
 
   def test_pull_bytes_truncated
+    buf = Raioquic::Buffer.new(capacity: 0)
+    assert_raises Raioquic::Buffer::BufferReadError do
+      buf.pull_bytes(2)
+    end
+    assert_equal 0, buf.tell
   end
 
   def test_pull_bytes_zero
+    buf = Raioquic::Buffer.new(data: "\x08\x07\x06\x05\x04\x03\x02\x01")
+    assert_equal "", buf.pull_bytes(0)
   end
 
   def test_pull_uint8
+    buf = Raioquic::Buffer.new(data: "\x08\x07\x06\x05\x04\x03\x02\x01")
+    assert_equal 0x08, buf.pull_uint8
+    assert_equal 1, buf.tell
   end
 
   def test_pull_uint8_truncated
+    buf = Raioquic::Buffer.new(capacity: 0)
+    assert_raises Raioquic::Buffer::BufferReadError do
+      buf.pull_uint8
+    end
+    assert_equal 0, buf.tell
   end
 
   def test_pull_uint16
+    buf = Raioquic::Buffer.new(data: "\x08\x07\x06\x05\x04\x03\x02\x01")
+    assert_equal 0x0807, buf.pull_uint16
+    assert_equal 2, buf.tell
   end
 
   def test_pull_uint16_truncated
+    buf = Raioquic::Buffer.new(capacity: 1)
+    assert_raises Raioquic::Buffer::BufferReadError do
+      buf.pull_uint16
+    end
+    assert_equal 0, buf.tell
   end
 
   def test_pull_uint32
+    buf = Raioquic::Buffer.new(data: "\x08\x07\x06\x05\x04\x03\x02\x01")
+    assert_equal 0x08070605, buf.pull_uint32
+    assert_equal 4, buf.tell
   end
 
   def test_pull_uint32_truncated
+    buf = Raioquic::Buffer.new(capacity: 3)
+    assert_raises Raioquic::Buffer::BufferReadError do
+      buf.pull_uint32
+    end
+    assert_equal 0, buf.tell
   end
 
   def test_pull_uint64
+    buf = Raioquic::Buffer.new(data: "\x08\x07\x06\x05\x04\x03\x02\x01")
+    assert_equal 0x0807060504030201, buf.pull_uint64
+    assert_equal 8, buf.tell
   end
 
   def test_pull_uint64_truncated
+    buf = Raioquic::Buffer.new(capacity: 7)
+    assert_raises Raioquic::Buffer::BufferReadError do
+      buf.pull_uint64
+    end
+    assert_equal 0, buf.tell
   end
 
   def test_push_bytes
+    buf = Raioquic::Buffer.new(capacity: 3)
+    buf.push_bytes("\x08\x07\x06")
+    assert_equal "\x08\x07\x06", buf.data
+    assert_equal 3, buf.tell
   end
 
   def test_push_bytes_truncated
+    buf = Raioquic::Buffer.new(capacity: 3)
+    assert_raises Raioquic::Buffer::BufferWriteError do
+      buf.push_bytes("\x08\x07\x06\x05")
+    end
+    assert_equal 0, buf.tell
   end
 
   def test_push_bytes_zero
+    buf = Raioquic::Buffer.new(capacity: 3)
+    buf.push_bytes("")
+    assert_equal "", buf.data
+    assert_equal 0, buf.tell
   end
 
   def test_push_uint8
+    buf = Raioquic::Buffer.new(capacity: 1)
+    buf.push_uint8(0x08)
+    assert_equal "\x08", buf.data
+    assert_equal 1, buf.tell
   end
 
   def test_push_uint16
+    buf = Raioquic::Buffer.new(capacity: 2)
+    buf.push_uint16(0x0807)
+    assert_equal "\x08\x07", buf.data
+    assert_equal 2, buf.tell
   end
 
   def test_push_uint32
+    buf = Raioquic::Buffer.new(capacity: 4)
+    buf.push_uint32(0x08070605)
+    assert_equal "\x08\x07\x06\x05", buf.data
+    assert_equal 4, buf.tell
   end
 
   def test_push_uint64
+    buf = Raioquic::Buffer.new(capacity: 8)
+    buf.push_uint64(0x0807060504030201)
+    assert_equal "\x08\x07\x06\x05\x04\x03\x02\x01", buf.data
+    assert_equal 8, buf.tell
   end
 
   def test_seek
+    buf = Raioquic::Buffer.new(data: "01234567")
+    assert_equal false, buf.eof
+    assert_equal 0, buf.tell
+
+    buf.seek(4)
+    assert_equal false, buf.eof
+    assert_equal 4, buf.tell
+
+    buf.seek(8)
+    assert_equal true, buf.eof
+    assert_equal 8, buf.tell
+
+    assert_raises Raioquic::Buffer::BufferReadError do
+      buf.seek(-1)
+    end
+    assert_equal 8, buf.tell
+
+    assert_raises Raioquic::Buffer::BufferReadError do
+      buf.seek(9)
+    end
+    assert_equal 8, buf.tell
+  end
+
+  def roundtrip(data, value)
+    data = (+data).force_encoding(Encoding::ASCII_8BIT)
+    buf = Raioquic::Buffer.new(data: data)
+    assert_equal value, buf.pull_uint_var
+    assert_equal data.size, buf.tell
+
+    buf = Raioquic::Buffer.new(capacity: 8)
+    buf.push_uint_var(value)
+    assert_equal data, buf.data
+  end
+
+  def test_uint_var
+    # 1 byte
+    roundtrip("\x00", 0)
+    roundtrip("\x01", 1)
+    roundtrip("\x25", 37)
+    roundtrip("\x3f", 63)
+
+    # 2 bytes
+    roundtrip("\x7b\xbd", 15293)
+    roundtrip("\x7f\xff", 16383)
+
+    # 4 bytes
+    roundtrip("\x9d\x7f\x3e\x7d", 494878333)
+    roundtrip("\xbf\xff\xff\xff", 1073741823)
+
+    # 8 bytes
+    roundtrip("\xc2\x19\x7c\x5e\xff\x14\xe8\x8c", 151288809941952652)
+    roundtrip("\xff\xff\xff\xff\xff\xff\xff\xff", 4611686018427387903)
+  end
+
+  def test_pull_uint_var_truncated
+    buf = Raioquic::Buffer.new(capacity: 8)
+    assert_raises Raioquic::Buffer::BufferReadError do
+      buf.pull_uint_var
+    end
+
+    buf = Raioquic::Buffer.new(data: "\xff")
+    assert_raises Raioquic::Buffer::BufferReadError do
+      buf.pull_uint_var
+    end
+  end
+
+  def test_push_uint_var_too_big
+    buf = Raioquic::Buffer.new(capacity: 8)
+    assert_raises Raioquic::Buffer::ValueError, "Integer is too big for a variable-length integer" do
+      buf.push_uint_var(4611686018427387904)
+    end
+  end
+
+  def test_size_uint_var
+    buf = Raioquic::Buffer.new
+    assert_equal 1, buf.size_uint_var(1)
+    assert_equal 2, buf.size_uint_var(16383)
+    assert_equal 4, buf.size_uint_var(1073741823)
+    assert_equal 8, buf.size_uint_var(4611686018427387903)
+    assert_raises Raioquic::Buffer::ValueError, "Integer is too big for a variable-length integer" do
+      buf.size_uint_var(4611686018427387904)
+    end
   end
 end
