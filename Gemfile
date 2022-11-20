84427325f094f5d73f87d44172d64bd97ffb8c3e
Yusuke Nakamura <yusuke1994525@gmail.com>
2022-11-20 14:18:29 +0900

    Colorize minitest result

--- Gemfile
+++ Gemfile
@@ -9,6 +9,8 @@ gem "rake", "~> 13.0"
 
 gem "minitest", "~> 5.0"
 
+gem "minitest-reporters"
+
 gem "rubocop", "~> 1.21"
 
 gem "steep"
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,3 +4,5 @@ $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "raioquic"
 
 require "minitest/autorun"
+require "minitest/reporters"
+Minitest::Reporters.use! Minitest::Reporters::DefaultReporter.new
