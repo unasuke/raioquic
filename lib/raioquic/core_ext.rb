9b3748d7a58622e725bb28c075fe4843db5bb8bc
Yusuke Nakamura <yusuke1994525@gmail.com>
2022-12-28 03:19:47 +0900

    Create Integer#to_bytes as core ext
    Subset of python's int.to_bytes

--- lib/raioquic/core_ext.rb
+++ lib/raioquic/core_ext.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 # CoreExt (like active support)
+
+# CoreExt for String class
 class String
   # Convert big endian bytes to integer (unsigned, up to 4 byte)
   def bytes_to_int
@@ -17,6 +19,36 @@ class String
       unpack1("N*").to_i
     else
       raise ArgumentError, "#{bytesize} bytes is not supported"
+    end
+  end
+end
+
+# CoreExt for Integer class
+class Integer
+  # Convert integer to specific byte size string (unsigned, big endian)
+  def to_bytes(bytesize) # rubocop:disable Metrics/CyclomaticComplexity
+    raise ArgumentError if self > 4294967295 # 5 bytes
+
+    template = case bytesize
+               when 0
+                 nil
+               when 1
+                 "C*"
+               when 2
+                 "n*"
+               when 3..4
+                 "N*"
+               else
+                 raise ArgumentError, "#{bytesize} bytes is too big"
+               end
+    return "" if template.nil?
+
+    [self].pack(template).then do |str|
+      if bytesize == 3
+        str.byteslice(1, 3).to_s
+      else
+        str
+      end
     end
   end
 end
--- test/raioquic/test_core_ext.rb
+++ test/raioquic/test_core_ext.rb
@@ -1,10 +1,11 @@
+# coding: ASCII-8BIT
 # frozen_string_literal: true
 
 require "test_helper"
 require "raioquic/core_ext"
 
 class TestRaioquicCoreExt < Minitest::Test
-  def test_bytes_to_int
+  def test_string_bytes_to_int
     assert_equal 0,        "".bytes_to_int
     assert_equal 3,        "\x03".bytes_to_int
     assert_equal 15,       "\x0f".bytes_to_int
@@ -15,6 +16,22 @@ class TestRaioquicCoreExt < Minitest::Test
 
     assert_raises ArgumentError do
       "\x01\x02\x03\x04\x05".bytes_to_int
+    end
+  end
+
+  def test_integer_to_bytes
+    assert_equal "", 0.to_bytes(0)
+    assert_equal "\x00", 0.to_bytes(1)
+    assert_equal "\x00\x00", 0.to_bytes(2)
+    assert_equal "\x03", 3.to_bytes(1)
+    assert_equal "\x0f", 15.to_bytes(1)
+    assert_equal "\x00\x10", 16.to_bytes(2)
+    assert_equal "\xfc\x00", 64512.to_bytes(2)
+    assert_equal "\x01\x02\x03", 66051.to_bytes(3)
+    assert_equal "\x01\x02\x03\x04", 16909060.to_bytes(4)
+
+    assert_raises ArgumentError do
+      4328719365.to_bytes(5)
     end
   end
 end
